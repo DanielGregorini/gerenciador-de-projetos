5a102a2b9ed8c614fca61c9aceabe48a96314383
Daniel Mahl Gregorini <danielmagregorini123@gmail.com>
2024-01-04 09:11:50 -0300

    inicial

---
+++ costs/src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+     
+        <p>costs</p>
+    </div>
+  );
+}
+
+export default App;
