e0dfeb3d8f5388d336ba9d81d301c1becd4177be
Daniel Mahl Gregorini <danielmagregorini123@gmail.com>
2024-01-06 08:21:45 -0300

    Pagina new project

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,5 +1,16 @@
+import ProjectForm from '../projects/ProjectForms'
+
+import styles from './NewProject.module.css'
+
 function NewProjet(){
-    return <h1>NewProjet</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar paragrafo</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+
+            <ProjectForm/>
+        </div>
+    )
 }
 
 export default NewProjet
