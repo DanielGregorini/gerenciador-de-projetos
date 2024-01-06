df6245166171e8b85b1156b50606193a762d53b3
Daniel Mahl Gregorini <danielmagregorini123@gmail.com>
2024-01-06 20:51:55 -0300

    input componente

--- costs/src/components/projects/ProjectForms.js
+++ costs/src/components/projects/ProjectForms.js
@@ -1,25 +1,35 @@
+import styles from './ProjectForm.module.css'
+
+import Input from '../form/Input'
 
 function ProjectForm(){
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto"/>
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total"/>
-            </div>
+        <form className={styles.form}>
+            
+            <Input 
+                type="text" 
+                text="Nome do projeto"
+                name = "name"
+                placeholder="Insira o nome do projeto"
+            />
+
+            <Input 
+                type="number" 
+                text="Orçamento do projeot"
+                name = "budget"
+                placeholder="Insira o orçamento total do projeto"
+            />
+
             <div>
                 <select name="category_id">
                     <option disabled>Selecione a Cagetoria</option>
                 </select>
             </div>
+
             <div>
                 <input type="submit" value="Criar projeto"/>
             </div>
            
-
-
-
         </form>
     )
 }
